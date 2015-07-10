a567caf44e6a55f25bad1b5c758daa7e8eb59dbf
Aaron Ponti <aarpon@gmail.com>
2015-07-10 17:56:33 +0200

    Initial import.

---
+++ core-plugins/shared/1/as/webapps/project-viewer/html/js/datamodel.js
@@ -0,0 +1,26 @@
+/**
+ * DataModel class
+ * 
+ * @author Aaron Ponti
+ *
+ */
+
+
+/**
+ * Define a model class to access all project related information.
+ */
+function DataModel() {
+
+    "use strict";
+
+    // Create a context object to access the context information
+    this.context = new openbisWebAppContext();
+
+    // Create an OpenBIS facade to call JSON RPC services
+    this.openbisServer = new openbis("/openbis");
+    
+    // Reuse the current sessionId that we received in the context for
+    // all the facade calls
+    this.openbisServer.useSession(this.context.getSessionId());
+
+}
---
+++ core-plugins/shared/1/as/webapps/project-viewer/html/js/dataviewer.js
@@ -0,0 +1,15 @@
+/**
+ * DataViewer class
+ * 
+ * @author Aaron Ponti
+ *
+ */
+
+/**
+ * A viewer to display DataModel entities to the html page.
+ */
+function DataViewer() {
+
+    "use strict";
+
+}
