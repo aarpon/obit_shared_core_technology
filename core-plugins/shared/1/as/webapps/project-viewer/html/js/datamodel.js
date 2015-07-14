891308e04ce871fa968677ec47f4630797424400
Aaron Ponti <aarpon@gmail.com>
2015-07-14 11:33:17 +0200

    Initial basic and incomplete implementation.

--- core-plugins/shared/1/as/webapps/project-viewer/html/js/datamodel.js
+++ core-plugins/shared/1/as/webapps/project-viewer/html/js/datamodel.js
@@ -23,4 +23,50 @@ function DataModel() {
     // all the facade calls
     this.openbisServer.useSession(this.context.getSessionId());
 
+    // Store the data
+    this.data = [];
+
+    // Retrieve all projects
+    this.openbisServer.listProjects(function(response) {
+        if (response.error) {
+
+            // Make sure that the data property is empty
+            DATAMODEL.data = [];
+
+            // Report the error
+            DATAVIEWER.displayStatus("Could not retrieve the list of projects!", "error");
+
+        } else {
+
+            // Build the data structure
+            DATAMODEL.buildDataStructure(response.result);
+
+        }
+    })
 }
+
+/**
+ * Build the data structure.
+ * @param projects array of projects.
+ */
+DataModel.prototype.buildDataStructure = function(projects) {
+
+    // Go over all projects and rearrange them per space
+    for (var i = 0; i < projects.length; i++) {
+
+        // Get the space code
+        var spaceCode = projects[i].spaceCode;
+
+        // Is the space already in the data array?
+        if (! DATAMODEL.data.hasOwnProperty(spaceCode)) {
+            DATAMODEL.data[spaceCode] = new Array();
+        }
+
+        // Add the project
+        DATAMODEL.data[spaceCode].push(projects[i]);
+    }
+
+    // Now display the data
+    DATAVIEWER.displayProjects(DATAMODEL.data);
+
+};
--- core-plugins/shared/1/as/webapps/project-viewer/html/js/dataviewer.js
+++ core-plugins/shared/1/as/webapps/project-viewer/html/js/dataviewer.js
@@ -13,3 +13,98 @@ function DataViewer() {
     "use strict";
 
 }
+
+/**
+ * Display status text color-coded by level.
+ * @param status: text to be displayed
+ * @param level: one of "success", "info", "warning", "error". Default is
+ * "info"
+ */
+DataViewer.prototype.displayStatus = function(status, level) {
+
+    // Display the status
+    $("#status").empty();
+
+    switch (level) {
+        case "success":
+            cls = "success";
+            break;
+        case "info":
+            cls = "info";
+            break;
+        case "warning":
+            cls = "warning";
+            break;
+        case "error":
+            cls = "danger";
+            break;
+        default:
+            cls = "info";
+            break;
+    }
+
+    status = "<div class=\"alert alert-" + cls + " alert-dismissable\">" +
+        status + "</div>";
+    $("#status").html(status);
+
+};
+
+/**
+ * Display the data.
+ * @param projects array of projects.
+ */
+DataViewer.prototype.displayProjects = function(data) {
+
+    // Display the status
+    $("#projects").empty();
+
+    // Get the spaces
+    var spaces = Object.keys(data);
+
+    for (var i = 0; i < spaces.length; i++) {
+
+        // Display the space
+        var s = $("<div>").addClass("space").text(spaces[i]);
+        $("#projects").append(s);
+
+        // Display all its projects
+        for (var j = 0; j < data[spaces[i]].length; j++) {
+
+            var project = data[spaces[i]][j];
+
+            var p = $("<div>").addClass("project").text(project.code).css('cursor', 'pointer').click(
+                DATAVIEWER.retrieveProjectInfo(project));
+
+            $("#projects").append(p);
+
+        }
+
+    }
+
+};
+
+/**
+ * Retrieve the information for the requested project.
+ * @param project A project
+ */
+DataViewer.prototype.retrieveProjectInfo = function(project) {
+
+    return function() {
+        DATAVIEWER.displayExperiments(project.code);
+    }
+
+};
+
+/**
+ * Display the data.
+ * @param projects array of projects.
+ */
+DataViewer.prototype.displayExperiments = function(experiments) {
+
+    // Display the status
+    $("#experiments").empty();
+
+    var p = $("<div>").addClass("experiment").text(experiments).css('cursor', 'pointer');
+    $("#experiments").append(p);
+
+};
